50b9fbd42cb283ed72beebc9836f44ca2c5c07d7
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-06-30 19:20:30 +0900

    refactor service-worker

--- service-worker.js
+++ service-worker.js
@@ -3,46 +3,56 @@ const STATIC_CACHE_NAME = 'static_' + VERSION;
 const ORIGIN = `${location.protocol}//${location.hostname}${(location.port ? ':' + location.port : '')}`
 const STATIC_FILES = [
   `${ORIGIN}/`,
+  `${ORIGIN}/index.html`,
   `${ORIGIN}/audio/09_TOEIC2_tr09.mp3`
 ]
-const STATIC_FILE_URL_HASH = new Set()
-STATIC_FILES.forEach((x) => STATIC_FILE_URL_HASH.add(x))
+const STATIC_FILE_URLS = new Set(STATIC_FILES)
 
+// Cache alll static files
 self.addEventListener('install', (evt) => {
+  console.log('install ');
   evt.waitUntil(
-    caches.open(STATIC_CACHE_NAME)
-    .then((cache) => {
-      return Promise.all(STATIC_FILES.map((url) => {
-        return fetch(new Request(url))
-          .then((response) => {
-            if (response.ok) {
-              return cache.put(response.url, response);
-            }
-
-            return Promise.reject(`Invalid response.  URL: ${response.url} Status: ${response.status}`)
-          })
-      }))
-    }))
+    caches
+    .open(STATIC_CACHE_NAME)
+    .then(cacheAllFiles)
+  )
 })
 
+function cacheAllFiles(cache) {
+  return Promise.all(STATIC_FILES
+    .map((url) => fetch(new Request(url))
+      .then((res) => saveResposeToCache(cache, res))
+    ))
+}
+
+function saveResposeToCache(cache, response) {
+  if (response.ok) {
+    return cache.put(response.url, response)
+  }
+
+  return Promise.reject(`Invalid response.  URL: ${response.url} Status: ${response.status}`)
+}
+
+// Return cache if cached
 self.addEventListener('fetch', (evt) => {
-  if (!STATIC_FILE_URL_HASH.has(evt.request.url)) {
+  const matched = caches.match(evt.request, {
+    cacheName: STATIC_CACHE_NAME
+  })
+
+  if (!matched) {
     return
   }
 
-  evt.respondWith(caches.match(evt.request, {
-    cacheName: STATIC_CACHE_NAME
-  }))
+  evt.respondWith(matched)
 })
 
+// Clean old caches
+// https://developer.mozilla.org/ja/docs/Web/API/ServiceWorker_API/Using_Service_Workers
+// > onactivateの基本的な使用法は、以前のバージョンのService Workerスクリプトで使用したリソースのクリーンアップです。
 self.addEventListener('activate', (evt) => {
   evt.waitUntil(
     caches.keys()
-    .then((keys) => {
-      const deleteCachePromises = keys
-        .filter((cacheName) => cacheName != STATIC_CACHE_NAME)
-        .map((cacheName) => caches.delete(cacheName))
-
-      return Promise.all(deleteCachePromises);
-    }))
+    .then((cachedURLs) => Promise.all(cachedURLs
+      .filter((url) => url != STATIC_CACHE_NAME)
+      .map((already_unused) => caches.delete(already_unused)))))
 })
