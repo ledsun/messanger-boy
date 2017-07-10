4bc6cabe6047e38655828c3f1c22dc41763506f3
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-11 00:47:43 +0900

    try

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,5 @@
 const VERSION = 1
 const STATIC_CACHE_NAME = 'static_' + VERSION;
-const ORIGIN = `${location.protocol}//${location.hostname}${(location.port ? ':' + location.port : '')}/messenger-boy`
-const STATIC_FILES = [
-  `${ORIGIN}/`,
-  `${ORIGIN}/index.html`,
-  `${ORIGIN}/audio/09_TOEIC2_tr09.mp3`,
-  `${ORIGIN}/audio/14_TOEIC2_tr14.mp3`,
-  `${ORIGIN}/audio/18_TOEIC2_tr18.mp3`,
-  `${ORIGIN}/audio/19_TOEIC2_tr19.mp3`,
-  `${ORIGIN}/audio/21_TOEIC2_tr21.mp3`,
-]
-const STATIC_FILE_URLS = new Set(STATIC_FILES)
 
 // Cache alll static files
 self.addEventListener('install', (evt) => {
@@ -18,11 +7,6 @@ self.addEventListener('install', (evt) => {
   evt.waitUntil(
     caches
     .open(STATIC_CACHE_NAME)
-    .then(cacheAllFiles)
+    .then((cache) => cache.addAll(['/', '/index.html']))
   )
 })
-
-function cacheAllFiles(cache) {
-  // return cache.addAll(STATIC_FILES)
-  return cache.addAll(['messenger-boy/', 'messenger-boy/index.html'])
-}
