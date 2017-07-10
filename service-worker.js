9f7396feb0f676417dd65c31fb61243959588233
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-11 00:35:24 +0900

    try

--- service-worker.js
+++ service-worker.js
@@ -36,11 +36,26 @@ function saveResposeToCache(cache, response) {
 
 // Return cache if cached
 self.addEventListener('fetch', (evt) => {
-  caches
-  .match(evt.request, {
-    cacheName: STATIC_CACHE_NAME
-  })
-  .then((response) => evt.respondWith(response))
+  event.respondWith(
+    caches
+    .open(STATIC_CACHE_NAME)
+    .then((cache) => cache.match(evt.request)
+      .then((response) => {
+        if (response) {
+          console.log(' Found response in cache:', response);
+
+          return response;
+        }
+      })
+      .catch(function(error) {
+
+        // match() か fetch() で発生する例外をハンドルする。
+        console.error('  Error in fetch handler:', error);
+
+        throw error;
+      })
+    )
+  )
 })
 
 // Clean old caches
