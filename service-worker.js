77523050a8ec7686235765176f6ec3deb1805486
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-07-11 01:00:46 +0900

    try

--- service-worker.js
+++ service-worker.js
@@ -10,26 +10,3 @@ self.addEventListener('install', (evt) => {
     .then((cache) => cache.addAll(['/messenger-boy/', '/messenger-boy/index.html']))
   )
 })
-
-self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-
-  event.respondWith(
-    caches.open(STATIC_CACHE_NAME).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        if (response) {
-          console.log(' Found response in cache:', response);
-
-          return response;
-        }
-        console.log('not found');
-      }).catch(function(error) {
-
-        // match() か fetch() で発生する例外をハンドルする。
-        console.error('  Error in fetch handler:', error);
-
-        throw error;
-      });
-    })
-  );
-});
